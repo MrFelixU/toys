0c874971b12256bca914eaaf6dd5f1639068144c
F Ulrich-Oltean <github@felixu.com>
2019-09-24 16:03:23 +0100

    Checked map, set up attack up to dice roll.

--- html5/frisk/logic.js
+++ html5/frisk/logic.js
@@ -11,6 +11,13 @@ var attacktarget = null;
 window.addEventListener("load", initGame);
 
 function initGame() {
+  // get buttons ready to listen
+  document.getElementById("btnattack").addEventListener("click", attackButtonClicked);
+  document.getElementById("btnendturn").addEventListener("click", endTurnClicked);
+
+  // can we play using this map?
+  if (!checkMap()) alert("Something is wrong with the map svg file");
+
   // grab the "country" elements from the svg
   loadCountries();
 
@@ -75,12 +82,10 @@ function initCountries() {
 
 function setupFirstPhase() {
   currentplayeridx = 0;
+  document.getElementById("player00").classList.add("turn");
   phase = "DEPLOY";
   armiestodeploy = 3;
-  logMessage(
-    "Player " + playernames[currentplayeridx] +
-    " to deploy " + armiestodeploy + " armies",
-    "action");
+  logMessage("Player " + playernames[currentplayeridx]+" to deploy " + armiestodeploy + " armies","action");
 }
 
 function updateInfo() {
@@ -103,7 +108,7 @@ function updateInfo() {
       p_el = document.getElementById(p_el_id);
       if (pcountries <= 0) p_el.setAttribute("class", "inactive"); // player is out
       p_el.getElementsByClassName("playerphase")[0].innerHTML =
-        (pidx==currentplayeridx ? phase : "idle");
+      (pidx==currentplayeridx ? phase : "idle");
       p_el.getElementsByClassName("playerarmies")[0].innerHTML = "Armies: " + playerstats.armies;
       p_el.getElementsByClassName("playercountries")[0].innerHTML = "Countries: " + playerstats.countries;
     }
@@ -113,7 +118,8 @@ function updateInfo() {
 
 function countryClicked(e){
   console.log("CLICKED!  We're in the "+phase+" phase with a target of "+e.target.id);
-  el_country = e.target;
+  var el_country = e.target;
+  var attackButton = document.getElementById("btnattack");
   if (phase == "DEPLOY") {
     if (el_country.getAttribute("data-ruler") == currentplayeridx) {
       el_country.setAttribute("data-armies", parseInt(el_country.getAttribute("data-armies")) + 1);
@@ -121,27 +127,81 @@ function countryClicked(e){
     }
     if (armiestodeploy <=0) {
       phase = "ATTACK";
+      document.getElementById("btnendturn").disabled = false;
       let pname = playernames[currentplayeridx];
       logMessage(`Player ${pname} ready to attack; click origin and target`, "action");
+
     }
-  }
-  if (phase == "ATTACK") {
+  } else if (phase == "ATTACK") {
     // has player already chosen where to attack from?
-    if (attackorigin && canAttack(attackorigin, e)) {
-      attacktarget = e;
-      let oname = attackorigin
-      document.getElementById("btnattack").value = "Attack "
+    if (attackorigin && canAttack(attackorigin, el_country)) {
+      attacktarget = el_country;
+      let oname = getCountryName(attackorigin);
+      let tname = getCountryName(attacktarget);
+      attackButton.value = `Attack ${tname} FROM ${oname}`;
+      attackButton.disabled = false;
+    } else if ( !(attackorigin) && canMountAttack(el_country)) {
+      // let's choose the attack origin, as long as
+      // we're clicking on a country that belongs to us
+      attackorigin = el_country;
+      attackButton.value = "Attack FROM " + getCountryName(el_country);
     }
   }
   updateInfo();
 }
 
 function attackButtonClicked() {
+  // work out how many dice are being used
+  no_of_att_dice = 0;
+  while (!no_of_att_dice) {
+    max_att = Math.min(3, parseInt(attackorigin.getAttribute("data-armies")) - 1);
+    var resp = parseInt(
+      prompt(`Attack with how may dice?  (Choose 1..${max_att} )`));
+    no_of_att_dice = ( resp > 0 && resp <= max_att ? resp : null );
+  }
+  no_of_def_dice = 0;
+  while (!no_of_def_dice) {
+    max_def = Math.min(2, parseInt(attacktarget.getAttribute("data-armies")));
+    var resp = parseInt(
+      prompt(`Defend with how may dice?  (Choose 1..${max_def} )`));
+    no_of_def_dice = ( resp > 0 && resp <= max_def ? resp : null );
+  }
+
+
+
+
+  // turn off button until they have selected another origin and target
+  document.getElementById("btnattack").disabled = true;
+}
+
+function endTurnClicked() {
 
 }
+
+function getCountryName(el) {
+  return svgroot.getElementById(el.id +"-name").textContent;
+}
+
+function canMountAttack(el) {
+  var currentPlayerOwnsIt = el.getAttribute("data-ruler") == currentplayeridx;
+  var hasArmies = parseInt(el.getAttribute("data-armies")) > 1;
+  return  (hasArmies && currentPlayerOwnsIt);
+}
+
+function canAttack(el_origin, el_target) {
+  // players can't attack their own territory
+  if (el_origin.getAttribute("data-ruler") == el_target.getAttribute("data-ruler")) return false;
+
+  // must have at least 2 armies
+  if (parseInt(el_origin.getAttribute("data-armies")) < 2) return false;
+
+  return true;
+}
+
+
 /* ****************************************************************************
 
-   UTILITY FUNCTIONS
+UTILITY FUNCTIONS
 
 **************************************************************************** */
 
@@ -191,6 +251,51 @@ function getNeighboursOf(c) {
     }
   }
   return neighbours;
+}
+
+function checkMap() {
+  var maproot = document.getElementById("mapobj").contentDocument;
+  var country_els = maproot.getElementsByClassName("country");
+
+  // check we have enough countries to play with
+  if (country_els.length < 2) {
+    console.log("Not enough countries to play with");
+    return false;
+  }
+
+  // go through each country and...
+  for (i=0;i<country_els.length; i++) {
+    var c = country_els[i];
+    console.log("Checking " + c.id);
+    // check it has an armies, a name and an owner element
+    if (!(maproot.getElementById(c.id+"-armies") &&
+         maproot.getElementById(c.id+"-ruler") &&
+         maproot.getElementById(c.id+"-name")
+       )) return false;
+    console.log("Found the right subelements");
+
+
+    // let's check that each declared neighbour has
+    // also chosen us as their neighbour
+    let neighbour_ids = c.getAttribute("data-neighbours");
+    if (neighbour_ids) {
+      neighbour_ids = neighbour_ids.split(",");
+      for (nidx = 0; nidx < neighbour_ids.length; nidx++) {
+        let n = maproot.getElementById(neighbour_ids[nidx].trim());
+        if (! n) return false;
+        if (n.getAttribute("data-neighbours").split(",").includes(c.id)) {
+          console.log("Our neighbour "+n.id+" has us ("+c.id+") as a neighbour.");
+        } else {
+          console.log(`Neighbour relation not mutual for ${c.id} and ${n.id}`);
+          return false;
+        }
+      }
+    } else {
+      return  false;
+    }
+  }
+  console.log("The map svg seems to be [ OK ].");
+  return true;
 }
 
 function logMessage(msg="", msgclass="") {
